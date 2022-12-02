bf476d02af99394a4bf4eb2e66bf627830cf69d4
oliiversion <olivxrkei101@gmail.com>
2022-12-02 13:12:17 -0500

    yo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = '
+document.querySelector("#app").innerHTML = `
     <h1>hi vite</h1> 
-    '<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a> '
+    '<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a> ';
+    
