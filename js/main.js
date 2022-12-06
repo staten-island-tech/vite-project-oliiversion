41e81867d613a58c850f9c7a1e213f6be16dd95c
oliiversion <olivxrkei101@gmail.com>
2022-12-06 13:13:27 -0500

    finally

--- js/main.js
+++ js/main.js
@@ -1,5 +1,61 @@
-import "../style/style.css";
+import { menu } from "";
+const d = new Date();
+var day = "";
 
-document.querySelector("#app").innerHTML = `
-    <h1>hi vite</h1> 
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a> `;
+switch (d.getDay()) {
+  case 0:
+    day = "monday";
+    break;
+}
+
+const menuDiv = document.querySelector("#menu");
+display(menu);
+
+document.querySelector("#all").addEventListener("click", function () {
+  menuDiv.innerHTML = "";
+  display(menu);
+});
+document.querySelector("#beef").addEventListener("click", function () {
+  menuDiv.innerHTML = "";
+  const filtered = menu.filter((item) => item.meat === "beef");
+  display(filtered);
+});
+document.querySelector("#pescatarian").addEventListener("click", function () {
+  menuDiv.innerHTML = "";
+  const filtered = menu.filter((item) => item.meat === "fish");
+  display(filtered);
+});
+document.querySelector("#vegetarian").addEventListener("click", function () {
+  menuDiv.innerHTML = "";
+  const filtered = menu.filter((item) => item.meat === "none");
+  display(filtered);
+});
+
+document.querySelector("#theme-button").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList = ["dark"];
+  } else {
+    document.body.classList = ["light"];
+  }
+});
+
+function display(array) {
+  console.log(day);
+  const withSales = array.map(sale);
+  withSales.forEach((item) => {
+    menuDiv.insertAdjacentHTML(
+      "beforeend",
+      `<div class="menu-object">
+                <h2>${item.name}</h2>
+                <img class="menu-image" src="${item.image}">
+                <p class="menu-object-price">$${item.price}</p>
+                <p class="menu-object-text">${item.description}</p>
+            </div>`
+    );
+  });
+}
+
+function sale(item) {
+  if (item.sales.includes(day)) item.price = item.price / 2;
+  return item;
+}
